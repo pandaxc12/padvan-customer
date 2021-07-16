289f991beffab070501ed2981d5391cc3a8015e9
pandaxc12 <11658347+pandaxc12@users.noreply.github.com>
2021-07-17 01:57:14 +0800

    Update mod_params.sh

--- mod_params.sh
+++ mod_params.sh
@@ -1,3 +1,4 @@
 cd /opt/rt-n56u
 sed -i 's/CONFIG_MTD_STORE_PART_SIZ=0x80000/CONFIG_MTD_STORE_PART_SIZ=0x600000/g' trunk/configs/boards/K2P/kernel-3.4.x.config
 sed -i 's/mtd_part_size=65536/mtd_part_size=6291456/g' trunk/user/scripts/mtd_storage.sh
+sed -i '/module_smart_load("xt_set", NULL);/amodule_smart_load("xt_TPROXY", NULL);' user/rc/rc.c
